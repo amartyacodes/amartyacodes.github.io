5d320312dbd66f1ef6238649aceb243f0b0673fa
Amartya <amartyab.ju@gmail.com>
2022-05-08 13:24:23 +0530

    my changes

--- db/db.js
+++ db/db.js
@@ -3,69 +3,69 @@ export default {
 		about: {
 			text: [
 				"Hi &#128075;",
-				"I'm Amartya Bhattacharya, undergraduate research assistant at Banerjee Lab, Arizona State University and currently a final year student at University of Calcutta.",
-				"I am a curious soul who loves to solve problems in  Deep Learning, Computer Vision,NLP and applications of AI in Healthcare through research work.",
-				"Also involved in competitive programming and love to know about global politics",
+				"I'm Vinay Somawat. I’m currently working as a Software Engineer at Biofourmis India specialized in JavaScript Applications. I did my undergrad in CS from NIT Warangal.",
+				"I'm a developer, geek and curious human besides being an OpenSource enthusiast. I have experience of delivering solutions for interesting problems from startup and industry space.",
+				"I love to read, listen to music, and travel.",
 			],
 		},
 		contact: {
 			text: [
-				"If you would like to get in touch with me, be it for a research project, job opportunity, or to just say hi, feel free to send me an email. My email address is: amartya.bhattacharya1729@gmail.com",
+				"If you would like to get in touch with me, be it for exploring a technology, a business, or to just say hi, feel free to send me an email. My email address is: vinaysomawat40@gmail.com",
 			],
 		},
 	},
 	skills: [
 		{
 			title: "Languages",
-			skillName: "Python, C++, MATLAB, JavaScript", 
+			skillName: "JavaScript, C++",
 			color: "1",
-			percentage: "85",
+			percentage: "70",
 		},
 		{
-			title: "Frameworks",
-			skillName: "TensorFlow, PyTorch, SQL",
+			title: "Frameworks/Libraries",
+			skillName: "Angular, Reactjs",
 			color: "6",
-			percentage: "90",
+			percentage: "50",
 		},
 		{
-			title: "Libraries",
-			skillName: "OpenCV, Keras, Scikit Learn",
+			title: "Backend",
+			skillName: "Nodejs, MongoDB",
 			color: "2",
-			percentage: "80",
+			percentage: "40",
 		},
 		{
 			title: "Clouds",
-			skillName: "AWS(EC2), Heroku, Netlify",
+			skillName: "AWS(EC2, S3), Heroku, Netlify",
 			color: "3",
 			percentage: "30",
 		},
 		{
 			title: "Design",
-			skillName: "HTML, SCSS",
+			skillName: "HTML, Bootstrap, SCSS",
 			color: "4",
-			percentage: "40",
-		},
-		{
-			title: "Version Control",
-			skillName: "GitHub",
-			color: "7",
-			percentage: "60",
-		},
-		{
-			title: "CS Core Subjects",
-			skillName: "Data Structures and Algorithms, DBMS, Computer Networks,Operating System",
-			color: "3",
 			percentage: "70",
 		},
 		{
-			title: "Applications of Deep Learning",
-			skillName: "Computer Vision",
-			color: "5",
-			percentage: "90",
+			title: "Version Control",
+			skillName: "GitHub, JIRA, Trello",
+			color: "7",
+			percentage: "70",
 		},
 		{
-			title: "Applications of Deep Learning",
-			skillName: "NLP",
+			title: "Tools",
+			skillName: "Postman, Chrome DevTools",
+			color: "3",
+			percentage: "80",
+		},
+		{
+			title: "Saas products",
+			skillName: "CleverTap, FreshDesk",
+			color: "5",
+			percentage: "50",
+		},
+		{
+			title: "Editor",
+			skillName: "VS Code",
 			color: "6",
 			percentage: "70",
 		},
@@ -73,184 +73,166 @@ export default {
 	projects: {
 		web: [
 			{
-				projectName: "ARAMBHML-An Auto ML Platform for Beginners",
-				image: "images/arambhml.png",
+				projectName: "Programming Diaries",
+				image: "images/programmingdiaries.png",
 				summary:
-					"Developed a beginner-friendly auto ML platform capable of solving classification tasks using CSV data",
-				preview: "https://pypi.org/project/ARAMBHML/",
+					"Developed a full stack blog application to provide content on techical topics across the internet with admin interface.",
+				preview: "https://programmingdiaries.herokuapp.com/",
 				techStack: [
-					"Python",
-					"Pandas",
-					"ScikitLearn",
-					"ML Algorithms",
-					"PyPi",
+					"Django",
+					"SQLite",
+					"Bootstrap",
+					"JavaScript",
+					"Heroku",
 				],
 			},
 			{
-				projectName: "End to End Dog Breed Classifier with Rest API",
-				image: "images/breed.png",
+				projectName: "Find Your Bank",
+				image: "images/findyourbank.png",
 				summary:
-					"Developed a Rest API application capable of classifying the breeds of dogs from the images",
-				preview: "https://github.com/amartyacodes/DogBreedClassification-",
-				techStack: ["Python", "Open CV", "TensorFlow", "Rest API"],
+					"Developed a React application to render a list of banks fetched from API. Filtered the banks based on queries from localstorage, marked favorites banks.",
+				preview: "https://clever-fermi-0d5d76.netlify.app",
+				techStack: ["Reactjs", "Bootstrap", "JavaScript", "Netlify"],
 			},
 			{
-				projectName: "Analysis of Machine Learning Algorithms for Prediction of Diabetes",
-				image: "images/diabetes.png",
+				projectName: "Web Portfolio",
+				image: "images/portfolio.png",
 				summary:
-					"Research paper on the analysis of ML algorithms for diabetes classification task and importance of each features, published on Taylor&Francis Group",
-				preview: "https://www.researchgate.net/publication/355902066_Analysis_of_Machine_Learning_Algorithms_for_Prediction_of_Diabetes",
-				techStack: ["Python", "Scikit-Learn"],
+					"Web Portfolio to showcase acadmics, skills, projects and contact details in better manner.",
+				preview: "https://github.com/vinaysomawat/Travographer-Portal",
+				techStack: ["HTML", "Bootstrap", "JavaScript"],
 			},
-
+			{
+				projectName: "Resume Builder",
+				image: "images/resume-builder.png",
+				summary:
+					"Browser based editor to build and download Resumes in a customizable templates.",
+				preview: "https://vinaysomawat.github.io/Resume-Builder",
+				techStack: ["HTML", "Bootstrap", "JavaScript"],
+			},
 		],
 		software: [
 			{
-				projectName: "A Novel Deep Learning Based Model for Classification of Rice Leaf Diseases",
-				image: "images/dlproject.png",
+				projectName: "Pizza Ordering ChatBot",
+				image: "images/pizzaorderchatbot.png",
 				summary:
-					"A research paper on rice leaf detection using deep learning model, published in IEEE Access",
+					"ChatBot using Dialogflow, Firebase database which stores the chat data in the realtime database.",
 				preview:
-					"https://ieeexplore.ieee.org/abstract/document/9638278",
-				techStack: ["Python", "TensorFlow"],
+					"https://github.com/vinaysomawat/Pizza-Ordering-ChatBot",
+				techStack: ["Dailogflow", "Firebase"],
 			},
 			{
-				projectName: "Application of Federated Learning in Building a Robust COVID-19 Chest X-ray Classification Model",
-				image: "images/Fl.jpg",
+				projectName: "WhatsApp-Bot",
+				image: "images/whatsappbot.jpg",
 				summary:
-					"Research Paper on application of Federated Learning in classification of COVID-19 chest X-rays, published on arxiv",
-				preview: "https://arxiv.org/abs/2204.10505",
-				techStack: ["Python", "TensorFlow Federated", "Federated Learning"],
+					"Python script which helps to send messages to WhatsApp contacts automatically using selenium and web automation.",
+				preview: "https://github.com/vinaysomawat/WhatsApp-Bot",
+				techStack: ["Selenium", "Chrome Webdriver", "Python"],
+			},
+			{
+				projectName: "Bill Generator",
+				image: "images/billgenerator.png",
+				summary:
+					"GUI to transfer data to excel sheets and generate bills on the local shops.",
+				preview: "https://github.com/vinaysomawat/Bill-Generator",
+				techStack: ["Tkinter", "Openxlpy", "Python"],
 			},
 		],
 		android: [
 			{
-				projectName: "Cell-Detection-Using-Image-Processing",
-				image: "images/cell1.jpg",
+				projectName: "NITW-CSE",
+				image: "images/nitwcse.jpg",
 				summary:
-					"An automated White Blood Cells(WBCs) detection technique from bone marrow",
-				preview: "https://github.com/amartyacodes/Cell-Detection-Using-Image-Processing",
-				techStack: ["Python", "Open CV"],
+					"The Application display details of Department courses, reference books, research, publication and faculty profile.",
+				preview: "https://github.com/vinaysomawat/NITW-CSE",
+				techStack: ["JAVA", "XML", "Android"],
 			},
 			{
-				projectName: "Exudate Segmentation from Retinal Images",
-				image: "images/exudate.png",
+				projectName: "CareerHigh-App",
+				image: "images/carrerhigh.png",
 				summary:
-					"An automated exudate segmentation technique from retinal images using image processing techniques",
-				preview: "https://github.com/amartyacodes/Exudate-Segmentation",
-				techStack: ["Python","Open CV"],
+					"The Application display the webpages of website careerhigh.in in android devices.",
+				preview: "https://github.com/vinaysomawat/CareerHigh-Android",
+				techStack: ["JAVA", "XML", "Android"],
 			},
-			{
-				projectName: "Multiclass-U-Net-for-Fetal-Feature-Extraction",
-				image: "images/munet.png",
-				summary:
-					"Built the Multiclass U-Net model from scratch for segmentation of Fetal Features",
-				preview: "https://github.com/amartyacodes/Multiclass-U-Net-for-Fetal-Feature-Extraction",
-				techStack: ["Python","TensorFlow","Numpy"],
-			},
-
 		],
 		freelance: [
 			{
-				projectName: "Unsupervised-WBC-Segmentation",
-				image: "images/cell2.png",
+				projectName: "SnylloAir.com",
+				image: "images/snylloair.png",
 				summary:
-					"Applied Unsupervised ML technique in order to segment White Blood Cells from bone marrow images.",
-				preview: "https://github.com/amartyacodes/Unsupervised-WBC-Segmentation",
-				techStack: ["Python","TensorFlow","Numpy"],
+					"Developed a company website to showcase the purpose, services and products provided by the company to audience.",
+				preview: "https://www.snylloair.com/",
+				techStack: ["Bootstrap", "JavaScript", "AWS-S3"],
+			},
+			{
+				projectName: "Delivery+",
+				image: "images/AM-Logo-.png",
+				summary:
+					"Android Application to display website in android devices.",
+				preview:
+					"https://play.google.com/store/apps/details?id=com.americanmarket.americanmarketandroid",
+				techStack: ["Android", "JAVA", "Play Store"],
 			},
 		],
 	},
 	experience: [
 		{
-			title: "Arizona State University: Banerjee Lab",
-			duration: "Dec 2021 - Present",
-			subtitle: "Undergraduate Research Assistant",
+			title: "Biofourmis India Pvt. Ltd.",
+			duration: "April 2022 - Present",
+			subtitle: "Software Engineer",
 			details: [
-				"Developed the code for conversion of ECG images to plots using Python and Matplotlib.",
-				"Developed a multimodal neural network model to predict adverse healthcare outcomes from ECG images and EHR Data."
+				"Coming soon.."
 			],
 			tags: [
-				"Python",
-				"MATLAB",
-				"Open CV",
-				"TensorFlow",
-				"LIME",
-				
+				"JavaScript",
+				"Angular",
+				"React",
+				"Bootstrap",
+				"Nodejs",
+				"Jenkins",
 			],
 			icon: "heartbeat",
 		},
 		{
-			title: "DeepTek Inc",
-			duration: "August 2021 - Feb 2022",
-			subtitle: "Machine Learning Intern",
+			title: "Novopay Solutions Pvt. Ltd.",
+			duration: "June 2020 - April 2022",
+			subtitle: "Software Engineer",
 			details: [
-				"Worked on Federated Learning(FL) algorithms and building a model using TensorFlow Federated for detecting diseases from CT scan and X ray images",
-				"Built a Deep Learning Model using a FL framework, from scratch for classification of Covid 19 Chest X-Ray images which increased the AUC score by 5% than the deep learning models trained without FL framework",
-			    "Paper Available on Arxiv/NASA ADS/Semantic Scholar"
-			], 
-			tags: [
-				"Python",
-				"TensorFlow",
-				"TFF",
-				"Grad-CAM",
+				"Supporting both frontend & backend teams for AEPS, DMT transaction, CMS Service, gateways.",
+				"Working on biometric eKYCs, CDM card and onboarding retailers flows.",
 			],
-			icon: "heartbeat",
+			tags: [
+				"JavaScript",
+				"Angular",
+				"React",
+				"Bootstrap",
+				"Nodejs",
+				"Jenkins",
+			],
+			icon: "qrcode",
 		},
 		{
-			title: "ISRO",
-			duration: "August 2021 - Dec 2021",
-			subtitle: "AI Research Intern",
+			title: "ThinkPedia LLP",
+			duration: "May 2019 - June 2019",
+			subtitle: "SDE Intern",
 			details: [
-				"Worked at MOSDAC under Dr.Bipasha Paul Shukla,on building a model for identification of seeding condition ofclouds using Machine Learning with Python",
-				"Analyzed the Australian Cloud Seeding Experimentresults using Python in orderto judge the factors that affected the cloud seeding experiments",
-			    "Analyzed and used unsupervised learning to create a model from Japan Aerospace Exploration Agency(JAXA) data to study the factors related to cloud seeding."
+				"Worked as a full stack developer to support tech team.",
+				"Developed a customer Web Application from scratch for social media management.",
 			],
-			tags: ["Python", "Scikit Learn", "Matplotlib", "Seaborn", "H5py"],
+			tags: ["JavaScript", "Angular", "Bootstrap", "Java", "Spring Boot"],
 			icon: "group",
-		},
-		{
-			title: "Center of Development of Advanced Computing(CDAC)",
-			duration: "May 2021 - July 2021",
-			subtitle: "Machine Learning Research Intern",
-			details: [
-				"Analysed the diabetes,stroke and heart attack detection data using Python,Matplotlib and Seaborn..",
-				"Built an Auto ML framework named ARAMBHML, capable of analysing the data and applying different machine learningalgorithms and showing their accuracy."
-			],
-			tags: [
-				"Python",
-				"Scikit-Learn"
-
-				
-			],
-			icon: "heartbeat",
-		},
-		{
-			title: "Data Science Lab: University of Calcutta",
-			duration: "March 2021 - Dec 2021",
-			subtitle: "Undergraduate Research Assistant",
-			details: [
-				"Underthe supervision of Dr.Amlan Chakrabarti, worked on deep learning based image segmentation techniques using Python, TensorFlow and Keras to segment cells from bone marrow slide images with over 97% dice coefficient score.",
-				"Built a Covolutional Neural Network model for classification of different types of cells present in the slide with an AUC score of 94.62%.",
-				"Built a Novel Instance Segmentation model which can segment cells from the slide and classify ."
-			],
-			tags: [
-				"Python",
-				"Open CV",
-				"TensorFlow"
-			],
-			icon: "heartbeat",
 		},
 	],
 	education: [
 		{
 			title: "Bachelors in Computer Science and Engineering",
-			duration: "2018-2022",
-			subtitle: "Universiy of Calcutta",
+			duration: "",
+			subtitle: "National Institute of Technology, Warangal",
 			details: [
-				"CGPA: 8.12",
-				"Active Competitive Programmer with CodeChef Rating 4*.",
-				"Kaggle 2x Expert with highest achieved rank of 497 out of 300,000 data scientists from around the world. ",
+				"Qualified GATE-2020.",
+				"Active Competitive Programmer with CodeChef Rating 1841*.",
+				"Received 500+ stars and 300+ forks on GitHub projects.",
 			],
 			tags: [
 				"Data Structures & Algorithms",
@@ -265,7 +247,7 @@ export default {
 		{
 			title: "Class 11-12th in Science and Mathematics",
 			duration: "",
-			subtitle: "South Point High School",
+			subtitle: "Board of Secondary Education, Rajasthan",
 			details: [
 				"Qualified JEE Advanced, Main & BITSAT.",
 				"Secured 99.5 percentile in Class 12th Boards Examinations.",
@@ -279,20 +261,16 @@ export default {
 			label: "Dev Profiles",
 			data: [
 				{
-					text: "Google Scholar",
-					link: "https://scholar.google.com/citations?user=0GiZhbkAAAAJ&hl=en",					
-				},
-				{
-					text: "Kaggle",
-					link: "https://www.kaggle.com/amartyabhattacharya",
+					text: "Stackoverflow",
+					link: "https://stackoverflow.com/users/8461233/vinay-somawat",
 				},
 				{
 					text: "GitHub",
-					link: "https://github.com/amartyacodes",
+					link: "https://github.com/vinaysomawat",
 				},
 				{
-					text: "Codechef",
-					link: "https://www.codechef.com/users/blaze1729",
+					text: "LeetCode",
+					link: "https://leetcode.com/somawatvinay/",
 				},
 			],
 		},
@@ -318,19 +296,22 @@ export default {
 			data: [
 				{
 					text: "Linkedin",
-					link: "https://www.linkedin.com/in/amartya-bhattacharya-1a4069175/",
+					link: "https://www.linkedin.com/in/vinaysomawat/",
 				},
 				{
-					text: "Whatsapp",
-					link: "+91-8910243949",
+					text: "Twitter",
+					link: "https://twitter.com/thesigmakid",
 				},
-
+				{
+					text: "Buy me a coffee",
+					link: "https://www.buymeacoffee.com/r194dME8y",
+				},
 			],
 		},
 		{
 			label: "copyright-text",
 			data: [
-				"Made with &hearts; by Amartya Bhattacharya with inspiration from Vinay Somawat.",
+				"Made with &hearts; by Vinay Somawat.",
 				"&copy; No Copyrights. Feel free to use this template."
 			],
 		},
